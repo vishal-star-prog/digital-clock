6495559e1916c04f4d46ddda91e38859b70bf356
vishal-star-prog <62098019+vishal-star-prog@users.noreply.github.com>
2021-02-14 16:46:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-function clock(){
+function showTime()
+{
     var hours = document.getElementById("hours");
     var minutes = document.getElementById("minutes");
     var seconds = document.getElementById("seconds");
+    var am_pm = document.getElementById("format");
     var h = new Date().getHours();
     var m = new Date().getMinutes();
     var s = new Date().getSeconds();
+    var am = "AM";
+    if(h>12){
+        h-=12;
+        var am = "PM";
+    }
+    if(h==0){
+        h=12;
+        var am = "AM";
+    }
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
     hours.innerHTML = h;
     minutes.innerHTML = m;
     seconds.innerHTML = s;
+    am_pm.innerHTML = am;
 }
-var interval = setInterval(clock,1000);
+setInterval(showTime,1000);
